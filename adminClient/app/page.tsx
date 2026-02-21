703a61123c625401891c29dd02fa7642fc759615
TOVO <tovojb101@gmail.com>
2026-02-21 21:44:19 +0300

    add dhasbord

--- adminClient/app/page.tsx
+++ adminClient/app/page.tsx
@@ -7,7 +7,7 @@ import { MainContent } from "@/components/dashboard/main-content";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Suspense } from "react";
 
-function DashboardContent() {
+function DashboardLayoutWrapper() {
   const searchParams = useSearchParams();
   const view = searchParams.get("view");
   const isExcalidrawView = view === "plan";
@@ -17,13 +17,13 @@ function DashboardContent() {
   }
 
   return (
-    <SidebarProvider className="bg-sidebar h-screen overflow-hidden">
+    <SidebarProvider className="bg-sidebar">
       <DashboardSidebar />
-      <div className="flex flex-col flex-1 h-full overflow-hidden bg-background">
-        <DashboardHeader />
-        <main className="flex-1 overflow-hidden relative">
+      <div className="h-svh overflow-hidden lg:p-2 w-full">
+        <div className="lg:border lg:rounded-md overflow-hidden flex flex-col items-center justify-start bg-container h-full w-full bg-background">
+          {view !== "bookmarks" && <DashboardHeader />}
           <MainContent />
-        </main>
+        </div>
       </div>
     </SidebarProvider>
   );
@@ -35,7 +35,7 @@ function DashboardContent() {
 export default function DashboardPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <DashboardContent />
+      <DashboardLayoutWrapper />
     </Suspense>
   );
 }
